d2a76bd675980e0a04636fc56bb3404288d19a59
Saba Phruidze <sabaphruidze01@gmail.com>
2025-12-11 13:02:26 +0400

    Visual and logic par in Users is done but I would like to do refactoring here since it is not as good visually as I want

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-//using Promise ,&& operator and forEach, table element as well
+//using Promise ,&& operator and forEach, table element as well,animate-pulse
 const Users = () => {
   interface Users {
     id: number;
@@ -11,6 +11,7 @@ const Users = () => {
   const [users, setUsers] = useState<Users[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [active, setActiveCount] = useState<number>(0);
+  const [error, setError] = useState<String | null>(null);
 
   useEffect(() => {
     const mockApiCall = new Promise<Users[]>((resolve, reject) => {
@@ -50,7 +51,57 @@ const Users = () => {
       });
   }, []);
 
-  return <div>Users</div>;
+  return (
+    <div className="bg-white p-8 rounded-lg shadow-md min-h-[25rem]">
+      <h2 className="text-3xl font-bold mb-6">Users</h2>
+      {/* when loading */}
+      {loading && <p className="text-blue-600 animate-pulse">Loading...</p>}
+      {/*when Error  */}
+      {error && (
+        <p className="text-red-600 font-bold border border-red-500 p-2 rounded">
+          ❌ {error}
+        </p>
+      )}
+      {/* when succed */}
+      {!loading && !error && (
+        <>
+          <h3 className="mb-4 flex items-center gap-2 text-green-700 font-bold leading-none">
+            <span className="inline-block w-2 h-2 rounded-full bg-green-700 animate-pulse" />
+            <span>Active Users: {active}</span>
+          </h3>
+
+          <table className="min-w-full border text-left">
+            <thead>
+              <tr>
+                <th className="p-2 border">ID</th>
+                <th className="p-2 border">Name</th>
+                <th className="p-2 border">Role</th>
+                <th className="p-2 border">Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((item) => (
+                <tr key={item.id} className="border-b">
+                  <td className="p-2">{item.id}</td>
+                  <td className="p-2">{item.name}</td>
+                  <td className="p-2">{item.role}</td>
+                  <td
+                    className={`${
+                      Object.is(item.status, "Active")
+                        ? "text-green-700"
+                        : "text-red-500"
+                    } `}
+                  >
+                    {item.status}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Users;
