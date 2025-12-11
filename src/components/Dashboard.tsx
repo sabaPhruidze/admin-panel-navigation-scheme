5e091b78556d43e4637bffda9de04f6666a146e7
Saba Phruidze <sabaphruidze01@gmail.com>
2025-12-11 11:19:09 +0400

    added tailwind as wel to make the web app good visual'

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return <div className="w-80 h-80 bg-red-500">Dashboard</div>;
 };
 
 export default Dashboard;
